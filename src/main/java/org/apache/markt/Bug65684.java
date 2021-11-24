0db50b8671e117506afa5789af9d00fec8a2998e
Mark Thomas <markt@apache.org>
2021-11-24 16:52:42 +0000

    Add test case for bug 65684
    https://bz.apache.org/bugzilla/show_bug.cgi?id=65684

---
+++ src/main/java/org/apache/markt/Bug65684.java
@@ -0,0 +1,44 @@
+package org.apache.markt;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.logging.LogManager;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Based on test case provided by roscoe@encoway.de
+ * https://bz.apache.org/bugzilla/show_bug.cgi?id=65684
+ */
+@WebServlet("/Bug65684")
+public class Bug65684 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    @Override
+    protected void doGet(HttpServletRequest request,
+                         HttpServletResponse response)
+            throws ServletException, IOException {
+
+        String s;
+        try {
+            s = CompletableFuture.supplyAsync(() -> {
+                LogManager lm = LogManager.getLogManager();
+                return lm.getProperty("anything");
+            }).get();
+        } catch (InterruptedException | ExecutionException e) {
+            response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+            PrintWriter printWriter = new PrintWriter(response.getOutputStream());
+            e.printStackTrace(printWriter);
+            printWriter.close();
+            return;
+        }
+
+        response.getOutputStream().println("Success: " + s);
+    }
+}
