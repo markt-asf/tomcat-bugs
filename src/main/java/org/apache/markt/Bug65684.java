ce58aafb889b8648ffd952e6fa8130bf7daa55d2
Mark Thomas <markt@apache.org>
2021-11-24 18:21:30 +0000

    Switch main to Servlet 6.0 (5.0 is latest supported by Eclipse)
    Tomcat 10.1.x / Java 11
    Port existing bugs

--- src/main/java/org/apache/markt/Bug65684.java
+++ src/main/java/org/apache/markt/Bug65684.java
@@ -6,11 +6,11 @@ import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 import java.util.logging.LogManager;
 
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 /**
  * Based on test case provided by roscoe@encoway.de
